f90dc36e982e94a9dc383d44c99127b18f18d887
Robopuffs <therobopuffs@gmail.com>
2025-02-02 15:28:32 -0800

    Renamed to Odometry Test file & cleared

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OdometryTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OdometryTest.java
@@ -2,13 +2,10 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@TeleOp(name="Test Encoders", group="TeleOps")
-public class EncoderTest extends LinearOpMode {
+@TeleOp(name="Odometry Test", group="TeleOps")
+public class OdometryTest extends LinearOpMode {
 
     @Override
     public void runOpMode() {
@@ -27,16 +24,6 @@ public class EncoderTest extends LinearOpMode {
 
             //******************************GAME FUNCTIONS******************************
             roboHardware.robotCentricDrive(gamepad1.right_stick_x, -gamepad1.right_stick_y, gamepad1.left_stick_x);
-
-            roboHardware.presetSlideLift(gamepad2.y, gamepad2.x, gamepad2.a);
-            telemetry.addData("Current Encoder Position:", roboHardware.extendMotor.getCurrentPosition());
-
-
-
-            if (gamepad2.left_stick_button) {
-                roboHardware.extendMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-                telemetry.addLine("Reset Encoder");
-            } //STOP & RESET ENCODER BUTTON
 
 
             telemetry.update(); //final updates for telemetry; displays all data added throughout the teleop loop
